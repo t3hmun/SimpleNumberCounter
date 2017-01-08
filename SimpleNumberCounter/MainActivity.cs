f8492fde1179cdd981d0be169ced8adf235310aa
t3hmun <t3hmun@gmail.com>
2017-01-08 15:36:32 +0000

    Count persist on configuration change via bundle.

--- SimpleNumberCounter/MainActivity.cs
+++ SimpleNumberCounter/MainActivity.cs
@@ -9,12 +9,18 @@ namespace SimpleNumberCounter
     {
         private int _count;
 
+        /// <summary>Launch MainActivity with Main layout and wire up UI button events.</summary>
+        /// <param name="bundle">Saved state from destruction - the count.</param>
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Get state from previously destroyed app.
+            if (bundle != null)
+                _count = bundle.GetInt("count");
 
             var plusOneButton = FindViewById<Button>(Resource.Id.Plus1Button);
             var minusOneButton = FindViewById<Button>(Resource.Id.Minus1Button);
@@ -23,6 +29,8 @@ namespace SimpleNumberCounter
             var resetButton = FindViewById<Button>(Resource.Id.ResetButton);
             var counterText = FindViewById<TextView>(Resource.Id.CountText);
 
+            ChangeCount(0, counterText);
+
             plusOneButton.Click += (sender, e) => { ChangeCount(1, counterText); };
             minusOneButton.Click += (sender, e) => { ChangeCount(-1, counterText); };
             plusFiveButton.Click += (sender, e) => { ChangeCount(5, counterText); };
@@ -30,10 +38,21 @@ namespace SimpleNumberCounter
             resetButton.Click += (sender, args) => { ChangeCount(-_count, counterText); };
         }
 
+        /// <summary>Changes count and updates UI.</summary>
+        /// <param name="amount">The amount to add to the count.</param>
+        /// <param name="counter">The count UI element.</param>
         private void ChangeCount(int amount, TextView counter)
         {
             _count += amount;
             counter.Text = _count.ToString();
+        }
+
+        /// <summary>Save state on destruction.</summary>
+        /// <param name="outState">The state save bundle</param>
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutInt("count", _count);
+            base.OnSaveInstanceState(outState);
         }
     }
 }
